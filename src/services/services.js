e74fff318d25db4e5c6578a5fec51b100104cb95
linker-luis <luis.dot444@gmail.com>
2022-01-19 23:51:19 -0500

    deploy

--- src/services/services.js
+++ src/services/services.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const serverUrl = 'http://127.0.0.1:8000/'
+// const serverUrl = 'http://127.0.0.1:8000/'
+const serverUrl = 'https://myfakeapibackend.herokuapp.com/'
 
 const apiPublic = axios.create({
     baseURL: serverUrl
