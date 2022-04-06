d75b304ea6863af1c97e7ac34bdd5b30dddd28ae
linker-luis <luis.dot444@gmail.com>
2022-04-06 00:06:48 -0500

    updated

--- src/services/services.js
+++ src/services/services.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // const serverUrl = 'http://127.0.0.1:8000/'
 const serverUrl = 'https://myfakeapibackend.herokuapp.com/'
-//updated--
+//updated---
 
 const apiPublic = axios.create({
     baseURL: serverUrl
